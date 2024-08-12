cf709ceeecd0a4a4c01a60044333784eb6c88d59
Robert <>
2024-08-13 01:48:16 +0700

    Scripts: Add BoardConfiguration
    BoardConfiguration is a configuration (scriptable object) that stores any data related to the match game, such as grid size, timer, etc..

---
+++ Assets/Scripts/BoardConfiguration.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(BoardConfiguration), menuName = "Matcher/" + nameof(BoardConfiguration))]
+public class BoardConfiguration : ScriptableObject
+{
+    [SerializeField, Min(1)] private int _width;
+    [SerializeField, Min(1)] private int _height;
+
+    public int Width
+    {
+        get { return _width; }
+    }
+
+    public int Height
+    {
+        get { return _height; }
+    }
+}

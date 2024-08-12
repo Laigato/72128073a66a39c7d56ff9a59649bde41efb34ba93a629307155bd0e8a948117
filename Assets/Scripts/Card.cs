96aa1e92f11f8acdb473c3a57a5be939aebafb1d
Robert <>
2024-08-13 02:57:07 +0700

    Scripts: Allow Card to reassign mesh shader
    Reassigned shader are material that would have its FrontTex replaced with loaded texture from CardData

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer _renderer;
+
     private CardDataSession _session;
 
     internal void Initialize(CardDataSession _toCardDataSession)
     {
         _session = _toCardDataSession;
+        _renderer.material = _session.Material;
     }
 
     public void Check()
